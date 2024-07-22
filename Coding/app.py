eb1cb04317a545aa56c8d4250cd1adb72c259b84
Pamala <pamala.r.cox@gmail.com>
2024-07-22 19:43:15 -0400

    Updated paths and fixed issues with data types

--- Coding/app.py
+++ Coding/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -9,7 +10,10 @@ def index():
 
 @app.route('/data')
 def data():
-    df = pd.read_csv('static/data/cleaned_df.csv')
+    csv_path = os.path.join(app.root_path, 'static', 'data', 'cleaned_df.csv')
+    df = pd.read_csv(csv_path)
+    df['Carbs (g)'] = pd.to_numeric(df['Carbs (g)'], errors='coerce')
+    df['Fiber (g)'] = pd.to_numeric(df['Fiber (g)'], errors='coerce')
     df['Net Carbs'] = df['Carbs (g)'] - df['Fiber (g)']
     return df.to_json(orient='records')
 
