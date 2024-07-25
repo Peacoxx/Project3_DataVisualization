baa44eeb302df194536033c3b29ecf062fb46043
Jrhaynes2 <jrhaynes2@gmail.com>
2024-07-24 20:21:04 -0400

    john's working app

--- Coding/app.py
+++ Coding/app.py
@@ -10,8 +10,8 @@ def index():
 
 @app.route('/data')
 def data():
-    # csv_path = os.path.join(app.root_path, 'static', 'data', 'cleaned_df.csv')
-    csv_path = ("../static/data/cleaned_df.csv")
+    csv_path = os.path.join(app.root_path, 'static', 'data', 'cleaned_df.csv')
+    # csv_path = ("../static/data/cleaned_df.csv")
     df = pd.read_csv(csv_path)
     df['Carbs (g)'] = pd.to_numeric(df['Carbs (g)'], errors='coerce')
     df['Fiber (g)'] = pd.to_numeric(df['Fiber (g)'], errors='coerce')
